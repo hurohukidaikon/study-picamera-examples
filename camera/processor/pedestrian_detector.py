df3880bc9a45113f4ff0390b6b4b064becf88c9b
hurohukidaikon <hurohukidaikon@gmail.com>
2018-10-24 20:22:17 +0900

    Update pedestrian_detector.py
    編集するファイルを間違っていたので元に戻した。

--- camera/processor/pedestrian_detector.py
+++ camera/processor/pedestrian_detector.py
@@ -3,42 +3,19 @@ from imutils.video.pivideostream import PiVideoStream
 from imutils.object_detection import non_max_suppression
 import imutils
 import time
+import datetime
 import numpy as np
 import cv2
-import os
-import sys
-import requests
 
 
-try:
-    SLACK_URL = os.environ['SLACK_URL']
-    SLACK_TOKEN = os.environ['SLACK_TOKEN']
-    SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
-except KeyError as e:
-    sys.exit('Couldn\'t find env: {}'.format(e))
-
-
-net = cv2.dnn.readNetFromCaffe('/home/pi/models/MobileNetSSD_deploy.prototxt',
-        '/home/pi/models/MobileNetSSD_deploy.caffemodel')
-
-
-def upload():
-    image = { 'file': open('hello.jpg', 'rb') }
-    payload = {
-        'filename': 'hello.jpg',
-        'token': SLACK_TOKEN,
-        'channels': [SLACK_CHANNEL],
-    }
-    requests.post(SLACK_URL, params=payload, files=image)
-
-
-class PersonDetector(object):
+class PedestrianDetector(object):
     def __init__(self, flip = True):
-        self.last_upload = time.time() # この行を新しく追加
         self.vs = PiVideoStream(resolution=(800, 608)).start()
         self.flip = flip
         time.sleep(2.0)
-
+        
+        self.hog = cv2.HOGDescriptor()
+        self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     def __del__(self):
         self.vs.stop()
 
@@ -54,37 +31,15 @@ class PersonDetector(object):
         return jpeg.tobytes()
 
     def process_image(self, frame):
-        frame = imutils.resize(frame, width=300)
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
-        net.setInput(blob)
-        detections = net.forward()
+        frame = imutils.resize(frame, width=min(500, frame.shape[1]))
+        
+        (rects, weights) = self.hog.detectMultiScale(frame, winStride=(8, 8),
+                padding=(8, 8), scale=1.1)
 
-        count = 0
-        for i in np.arange(0, detections.shape[2]):
-            confidence = detections[0, 0, i, 2]
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-            if confidence < 0.2:
-                continue
-
-            idx = int(detections[0, 0, i, 1])
-            if idx != 15:
-                continue
-
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype('int')
-            label = '{}: {:.2f}%'.format('Person', confidence * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            count += 1
-
-        if count > 0:
-            print('Count: {}'.format(count))
-            elapsed = time.time() - self.last_upload
-            if elapsed > 60:
-                cv2.imwrite('hello.jpg', frame)
-                upload()
-                self.last_upload = time.time()
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
         return frame
